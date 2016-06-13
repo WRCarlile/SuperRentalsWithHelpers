769988f81c8dd85593f4c6e6a4e9fbfe46fab9ce
Ned Faulhaber and Ryan Carlile <Guest@epicodus-31.local>
2016-06-13 10:58:48 -0700

    Add cost and popularity helper

---
+++ app/components/new-rental.js
@@ -0,0 +1,23 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewRental: false,
+  actions: {
+    rentalFormShow() {
+      this.set('addNewRental', true);
+    },
+    save1() {
+      var params = {
+        owner: this.get('owner'),
+        city: this.get('city') ? this.get('city') : "",
+        type: this.get('type') ? this.get('type') : "",
+        image: this.get('image') ? this.get('image') : "",
+        bedrooms: this.get('bedrooms') ? this.get('bedrooms') : "",
+        cost: parseInt(this.get('cost'))
+      };
+      console.log(params.city.get("name"));
+      this.set('addNewRental', false);
+      this.sendAction('rentalSave', params);
+    }
+  }
+});
