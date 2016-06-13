0a4f126eb45b5ccc268c97f0ad75f15fb2dd7846
Ned Faulhaber and Ryan Carlile <ryancarlile@gmail.com>
2016-06-13 11:40:05 -0700

    Add Full Location function

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,11 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    destroyCity(city) {
-      if (confirm('Are you sure you want to delete this city?')) {
-        this.sendAction('destroyCity', city);
-      }
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+    return this.get('city.name') + ', ' + this.get('city.country');
+  }),
+
+actions: {
+  destroyCity(city) {
+    if (confirm('Are you sure you want to delete this city?')) {
+      this.sendAction('destroyCity', city);
     }
   }
+}
 });
